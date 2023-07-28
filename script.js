6ddf96766ed104e4480478244e019c622e677aa7
Khan-Umar/ <130562593+umarkhan-1007@users.noreply.github.com>
2023-07-28 23:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+function init() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+init();
+
+var crsr = document.querySelector(".cursor")
+var main = document.querySelector(".main")
+document.addEventListener("mousemove",function(dets){
+  crsr.style.left = dets.x+ 5 +"px"
+  crsr.style.top = dets.y+ 5 +"px"
+})
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top 27%",
+        end: "top 0",
+        scrub: 3
+    }
+})
+tl.to(".page1 h1", {
+    x: -100,
+}, "anim")
+tl.to(".page1 h2", {
+    x: 100
+}, "anim")
+tl.to(".page1 video", {
+    width: "90%"
+}, "anim")
+
+var t2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top -50%",
+        end: "top -130%",
+        scrub: 3
+    }
+})
+
+t2.to(".main",{
+    backgroundColor:"#ffff"
+})
+
+var t3 = gsap.timeline({
+  scrollTrigger: {
+      trigger: ".page1 h1",
+      scroller: ".main",
+      // markers:true,
+      start: "top -500%",
+      end: "top -20%",
+      scrub: 3
+  }
+})
+
+t3.to(".main",{
+  backgroundColor:"#0F0D0D"
+})
+
+var boxs = document.querySelectorAll(".box")
+boxs.forEach(function(elem){
+  elem.addEventListener("mouseenter", function(){
+    var att = elem.getAttribute("data-imgae")
+    crsr.style.width = "300px"
+    crsr.style.height = "250px"
+    crsr.style.borderRadius = "0"
+    crsr.style.backgroundImage = `url(${att})`
+
+  })
+  elem.addEventListener("mouseleave", function(){
+    elem.style.backgroundColor = "transparent "
+    crsr.style.width = "20px"
+    crsr.style.height = "20px"
+    crsr.style.borderRadius = "50%"
+    crsr.style.backgroundImage = `none`
+
+  })
+})
+
+var h4 = document.querySelectorAll(".nav h4")
+var purple = document.querySelector("#purple")
+  h4.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+      purple.style.display = "block"
+      purple.style.opacity = "1"
+    })
+    elem.addEventListener("mouseleave", function(){
+      purple.style.display = "block"
+      purple.style.opacity = "0"
+    })
+  })
